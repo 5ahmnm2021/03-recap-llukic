c1315c9740c4f2a143e3ffb3956d44cee2cacd04
lealukic <lea.lukic@htl-salzburg.ac.at>
2021-03-02 09:27:17 +0100

    ignore,assets und projectS. added

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Master : MonoBehaviour
+{
+    public Light l;
+    public GameObject obj;
+    public BoxCollider boxC;
+    public Camera cam;
+    public Slider s;
+
+    void Start()
+    {
+        l.intensity = 1.4f;
+        obj.name = "Leuchte";
+        boxC.isTrigger = true;
+    }
+
+}
