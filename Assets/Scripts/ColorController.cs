45690a5d6831ba58288d7516c2a3a0cc5c1f5135
lealukic <lea.lukic@htl-salzburg.ac.at>
2021-03-22 09:43:48 +0100

    finalcommit of Assets ProjectSettings Readme und Gitignore

---
+++ Assets/Scripts/ColorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorController : MonoBehaviour
+{
+    public Image ColorField;
+    
+    public void ChangeColor()
+    {
+        int randomNumber;
+
+        var colors = new List<Color>()
+        {
+            new Color32(255, 205, 25, 100),
+            new Color32(95, 34, 0, 100),
+            new Color32(167, 235, 122, 100),
+            new Color32(245, 149, 235, 100)
+        };
+
+        if (Input.GetKeyDown("space"))
+        {
+            randomNumber = Random.Range(0, colors.Count);
+            ColorField.color = colors[randomNumber];
+        }
+    }
+    private void Update()
+    {
+        ChangeColor();
+    }
+}
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,36 +1,19 @@
-﻿//namespaces "bibliotheken"
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-//zeile 7 und 8 deklarieren den anfang der deklaration
 public class Controller : MonoBehaviour
 {
-    //variabeln sind speicherplatz für daten und können nicht ausserhalb von klassen deklariert werden
-    //variabeln deklarieren (exessmodifier - datentyp - variable)
-    //variabeln zuweisung = (name - zuweisungsoperator - wert)
-  
     public InputField Zahl01;
     public InputField Zahl02;
     public Text result;
     public Text Error;
-
     public float num1;
     public float num2;
 
-
-   // Start is called before the first frame update
-   // jetzt kommt die deklaration
-   // void Start()
-   //{
-   //hier steht der code der ausgeführt werden soll (z.B MyName(); = MethodenAufruf)
-   //}
-   // Update is called once per frame
-   // void Update()
-   //{
-   //}
+    string errorMsg = "Mate! Pls type a number!";
 
     public void ChangeSceneA()
     {
@@ -40,13 +23,11 @@ public class Controller : MonoBehaviour
     public void ChangeSceneB()
     {
         SceneManager.LoadScene("02NumberScene");
-     
     }
 
     public void ChangeSceneC()
     {
         SceneManager.LoadScene("00WelcomeScene");
-
     }
 
     public void AddNumbersOnClick()
@@ -58,7 +39,7 @@ public class Controller : MonoBehaviour
         catch (System.Exception)
         {
             Zahl01.image.color = new Color32(255,0,0,100);
-            Error.text = "Mate! You forgot something!";
+            Error.text = errorMsg;
         }
         try
         {
@@ -67,19 +48,11 @@ public class Controller : MonoBehaviour
         catch (System.Exception)
         {
             Zahl02.image.color = new Color32(255,0,0,100);
-            Error.text = "Mate! You forgot something!";
+            Error.text = errorMsg;
         }
 
-        //float addResult = num1 + num2;
         result.text = (num1 + num2).ToString();
         Debug.Log("Die Addition ergibt: " + result.text);
     }
 
-
 }
-
-
-//Klassen in Klassen deklarieren kann man NICHT
-//MethodenDeklaration besteht aus private string MyName(){}
-//void-> man bekommt nichts retour
-//strng--return; -> man bekommt was zurück
